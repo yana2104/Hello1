b1ffc48153ed23ba7ca78467b3c8acf1cfc591bd
yana2104 <127399920+yana2104@users.noreply.github.com>
2023-12-10 22:07:09 +0530

    Add files via upload

---
+++ Helllo.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class Helllo {
+    public static void main(String[] args) {
+        System.out.println("Hey how are you?");
+        Scanner s=new Scanner(System.in);
+        System.out.println("Number is:"+s.nextInt());
+    }
+}
